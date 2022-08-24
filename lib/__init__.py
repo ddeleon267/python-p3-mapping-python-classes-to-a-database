b9bc1f1acc9a020847f8e1f29e897520846ad1d5
ddeleon267 <dominique.a.deleon@gmail.com>
2022-08-24 17:28:45 -0400

    Done

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,5 @@
 import sqlite3
 
-CONN = sqlite3.connect('db/music.db')
-CURSOR = CONN.cursor()
+CONN = sqlite3.connect('db/music.db') # this obj represents connection to db
+
+CURSOR = CONN.cursor()  ## cursor needed to execure sql statements
